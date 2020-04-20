f1ea2076bb7bb23de793619edd806bacfd74ba8d
Filip Siwiec <filipsiwiec@gmal.com>
2020-04-20 17:13:10 +0200

    day 11 - another task

--- start.js
+++ start.js
@@ -1,11 +1,18 @@
-let pierwszy = document.querySelector(".s1");
-let drugi = document.querySelector(".s2");
 
-let funkcja = function(){
+let guzik = document.querySelector('button');
+let kwadrat = document.querySelector(".square");
+
+
+let obrot = 0;
+let zmienna = 10;
+kwadrat.style.transform = 'rotate(' + obrot + 'deg)';
+kwadrat.style.transition = "0.5s"
+
+let obracamy = function (){
     
-    pierwszy.classList.toggle('red');
-    drugi.classList.toggle("red")
-    console.log(this);
+    obrot += zmienna;
+    kwadrat.style.transform = 'rotate(' + obrot + 'deg)';
+    console.log(obrot);
 }
 
-pierwszy.addEventListener("click", funkcja);
+guzik.addEventListener("click", obracamy);
