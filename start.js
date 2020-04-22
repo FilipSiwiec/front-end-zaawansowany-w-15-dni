a5330354074a5b6aafcc4877767e4f7f57ebe2f5
Filip Siwiec <filipsiwiec@gmal.com>
2020-04-22 23:09:15 +0200

    day 13 - task 1e

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 // const ham = document.querySelector(".burger");
 // const side = document.querySelector("aside");
-
+// const wrap = document.querySelector(".wrapper");
 // const bar = document.querySelector(".fa-bars");
 // const x = document.querySelector(".fa-times");
 
@@ -9,9 +9,20 @@
 //     bar.classList.toggle("show");
 //     x.classList.toggle("show");
 //     side.classList.toggle("show");
+//     wrap.classList.toggle("show");
 // })
 
-$(".burger").on("click", function(){
-    console.log("dziala");
-    $(".fas, aside").toggleClass("show");
+// $(".burger").on("click", function(){
+//     console.log("dziala");
+//     $(".fas, aside, .wrapper").toggleClass("show");
+// })
+
+const ham = document.querySelector(".burger");
+const active = document.querySelectorAll(".active");
+
+ham.addEventListener("click", function(){
+    for ( i = 0; i < active.length; i++){
+        active[i].classList.toggle("show");
+    }
 })
+
