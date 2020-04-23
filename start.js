0e5d110e0a93cce8f89e29041de65e6dc50384fb
Filip Siwiec <filipsiwiec@gmal.com>
2020-04-23 11:22:38 +0200

    day 13 - project 2

--- start.js
+++ start.js
@@ -1,28 +1,4 @@
-// const ham = document.querySelector(".burger");
-// const side = document.querySelector("aside");
-// const wrap = document.querySelector(".wrapper");
-// const bar = document.querySelector(".fa-bars");
-// const x = document.querySelector(".fa-times");
-
-// ham.addEventListener("click", function(){
-//     // console.log("dziala");
-//     bar.classList.toggle("show");
-//     x.classList.toggle("show");
-//     side.classList.toggle("show");
-//     wrap.classList.toggle("show");
-// })
-
-// $(".burger").on("click", function(){
-//     console.log("dziala");
-//     $(".fas, aside, .wrapper").toggleClass("show");
-// })
-
-const ham = document.querySelector(".burger");
-const active = document.querySelectorAll(".active");
-
-ham.addEventListener("click", function(){
-    for ( i = 0; i < active.length; i++){
-        active[i].classList.toggle("show");
-    }
+$(".burger").on("click", function(){
+    // console.log("dziala");
+        $("i.fas, nav").toggleClass("show");
 })
-
